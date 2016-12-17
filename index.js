54686c6642903a400ddb623f6457b5a1fa7d1751
Pedro Felix <pmhsfelix@gmail.com>
2016-12-17 18:59:32 +0000

    adds alias and improves usage messages

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 'use latest'
 const request = require('request')
 const conceptsUrl = 'http://webconcepts.info/concepts.json'
+function useAlias(conceptType){
+  const alias = {
+    'method': 'http-method',
+    'status': 'http-status-code',
+    'relation': 'link-relation'
+  }
+  return alias[conceptType] || conceptType;
+}
 
 module.exports = function (ctx, cb) {
 
@@ -12,9 +20,14 @@ module.exports = function (ctx, cb) {
     });
   }
 
-  const sendUsage = () => {
-    return send(
-      `Usage: "{concept-type} {concept-name}", where {concept-type} can be: ${conceptList}`);
+  const sendUsage = (concepts) => {
+    let conceptList = concepts.map(c => c.concept).join(', ')
+    return send(`Usage: "{concept-type} {concept-name}", where {concept-type} can be: ${conceptList}`);
+  }
+
+  const sendUsageForUnknownType = (type, concepts) => {
+    let conceptList = concepts.map(c => c.concept).join(', ')
+    return send(`Unknown concept-type "${type}", usage: "{concept-type} {concept-name}", where {concept-type} can be: ${conceptList}`);
   }
 
   // authorize
@@ -23,46 +36,43 @@ module.exports = function (ctx, cb) {
     return cb(null,{})
   }
 
-  // check for empty ...
-  var text = ctx.data.text.trim();
-  if(!text){
-    return sendUsage()
-  }
-
-  // ... or invalid command
-  var parts = ctx.data.text.trim().split(' ');
-  if(parts.length != 2){
-    return sendUsage();
-  }
-
-  var conceptType = parts[0];
-  var conceptName = parts[1];
-
   request(conceptsUrl, (error, response, body) => {
     if (error || response.statusCode >= 500){
       return send('Unable to access the http://webconcepts.info, sorry');
     }
 
-    var concepts = JSON.parse(body);
-    var concept = concepts.filter(c => new RegExp('^' + conceptType + '$', 'i').test(c.concept))[0];
+    const concepts = JSON.parse(body);
 
-    if(!concept) {
-      var conceptList = concepts.reduce((list, c) => {
-        list += ', ' + c.concept;
-        return list;
-      }, '');
-      return send(`Unknown concept-type "${parts[0]}", usage: "{concept-type} {concept-name}", where {concept-type} can be: ${conceptList}`);
+    // check for empty ...
+    const text = ctx.data.text.trim();
+    if(!text){
+      return sendUsage(concepts);
     }
 
-    var conceptValue = concept.values.filter(c => new RegExp('^' + conceptName + '$', 'i').test(c.value))[0];
+    // ... or invalid command
+    const parts = ctx.data.text.trim().split(' ');
+    if(parts.length != 2){
+      return sendUsage(concepts);
+    }
+
+    const conceptType = useAlias(parts[0]);
+    const conceptName = parts[1];
+
+    const concept = concepts.filter(c => new RegExp('^' + conceptType + '$', 'i').test(c.concept))[0];
+
+    if(!concept) {
+      return sendUsageForUnknownType(conceptType, concepts);
+    }
+
+    const conceptValue = concept.values.filter(c => new RegExp('^' + conceptName + '$', 'i').test(c.value))[0];
 
     if(!conceptValue) {
       return send(`Unable to find the "${conceptName}" concept, sorry.`);
     }
 
-    var respText = conceptValue.details[0].description;
-    var spec = conceptValue.details[0].documentation;
-    var site = conceptValue.id;
+    const respText = conceptValue.details[0].description;
+    const spec = conceptValue.details[0].documentation;
+    const site = conceptValue.id;
     send(
         `*${conceptName}*:\n${respText}`,
         [
